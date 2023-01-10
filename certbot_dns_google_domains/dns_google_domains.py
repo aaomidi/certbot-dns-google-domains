7b2ee9a147c1730de6c712cf540b0e7a4fe93fa4
Amir Omidi <amir@aaomidi.com>
2023-01-10 20:19:34 +0000

    moar?

--- certbot_dns_google_domains/dns_google_domains.py
+++ certbot_dns_google_domains/dns_google_domains.py
@@ -1,25 +1,28 @@
 from typing import Callable, Optional, List
 from dataclasses import dataclass, field
-from dataclasses_json import dataclass_json, LetterCase, config
-import zope.interface
 import requests
+from dataclasses_json import LetterCase, config, DataClassJsonMixin
+import zope.interface
 
 from certbot import errors, interfaces
 from certbot.plugins import dns_common
 
 
-@dataclass_json(letter_case=LetterCase.CAMEL)
 @dataclass
-class AcmeTxtRecord:
+class AcmeTxtRecord(DataClassJsonMixin):
+    dataclass_json_config = config(letter_case=LetterCase.CAMEL)[
+        "dataclasses_json"]
     fqdn: str
     digest: str
     update_time: Optional[str] = field(default=None, metadata=config(
         exclude=lambda f: f is None))  # type: ignore
 
 
-@dataclass_json(letter_case=LetterCase.CAMEL)
 @dataclass
-class RotateChallengesRequest:
+class RotateChallengesRequest(DataClassJsonMixin):
+    dataclass_json_config = config(letter_case=LetterCase.CAMEL)[
+        "dataclasses_json"]
+
     access_token: str
     records_to_add: Optional[List[AcmeTxtRecord]] = field(default=None, metadata=config(
         exclude=lambda f: f is None))  # type: ignore
@@ -28,9 +31,10 @@ class RotateChallengesRequest:
     keep_expired_records: bool = False
 
 
-@dataclass_json(letter_case=LetterCase.CAMEL)
 @dataclass
-class AcmeChallengeSet:
+class AcmeChallengeSet(DataClassJsonMixin):
+    dataclass_json_config = config(letter_case=LetterCase.CAMEL)[
+        "dataclasses_json"]
     record: List[AcmeTxtRecord]
 
 
@@ -42,22 +46,14 @@ class GDSApi:
     def __init__(self, access_token: str):
         self.access_token = access_token
 
-    def rotate_challenges(self, domain: str, validation_name: str, validation_token: str) -> Optional[AcmeChallengeSet]:
-        record_add = AcmeTxtRecord(validation_name, validation_token)
-        rotate_req = RotateChallengesRequest(
-            self.access_token, [record_add], None, True)
+    def rotate_challenges(self, domain: str, rotate_req: RotateChallengesRequest) -> Optional[AcmeChallengeSet]:
         request_json = rotate_req.to_json()
-        print(request_json)
 
-        url = self.ROTATE_CHALLENGES.format(
-            domain=domain)
-        print(url)
+        url = self.ROTATE_CHALLENGES.format(domain=domain)
         result = requests.post(
             url, data=request_json, timeout=self.DEFAULT_TIMEOUT,
             headers={"Content-Type": "application/jso; charset=utf-8"})
-        print(result.text)
-        if result.status_code != 200:
-            return None
+        result.raise_for_status()
 
         return AcmeChallengeSet.from_json(result.text)
 
@@ -71,9 +67,6 @@ class Authenticator(dns_common.DNSAuthenticator):
 
     access_token: str
 
-    def __init__(self, *args, **kwargs):
-        super(Authenticator, self).__init__(*args, **kwargs)
-
     @classmethod
     def add_parser_arguments(cls, add: Callable[..., None], default_propagation_seconds: int = 30) -> None:
         super(Authenticator, cls).add_parser_arguments(
@@ -83,8 +76,7 @@ class Authenticator(dns_common.DNSAuthenticator):
     def _validate_credentials(self, credentials: dns_common.CredentialsConfiguration) -> None:
         self.access_token = credentials.conf('access-token')
         if not self.access_token:
-            raise errors.PluginError('{}: access_token was not found in the configuration for Google Domains.'.format(
-                credentials.confobj.filename))
+            raise errors.PluginError(errors.MisconfigurationError(f'{credentials.confobj.filename}: access_token was not found in the configuration for Google Domains.'))
 
     def _setup_credentials(self):
         self._configure_credentials(
@@ -94,14 +86,26 @@ class Authenticator(dns_common.DNSAuthenticator):
             self._validate_credentials
         )
 
-    def _perform(self, domain: str, validation_name: str, validation_token: str) -> None:
+    def _perform(self, domain: str, validation_name: str, validation: str) -> None:
         gds_api = self._get_gds_api()
-        result = gds_api.rotate_challenges(
-            domain, validation_name, validation_token)
-        print(result)
+        record_add = AcmeTxtRecord(validation_name, validation)
+        rotate_req = RotateChallengesRequest(
+            self.access_token, [record_add], None, True)
+        try:
+            gds_api.rotate_challenges(domain, rotate_req)
+        except Exception as err:
+            raise errors.PluginError(f"Unable to rotate DNS challenges: {err}")
 
     def _get_gds_api(self) -> GDSApi:
         return GDSApi(self.access_token)
 
-    def _cleanup(self, domain, validation_name, validation):
+    def _cleanup(self, domain: str, validation_name: str, validation: str):
+        gds_api = self._get_gds_api()
+        record_remove = AcmeTxtRecord(validation_name, validation)
+        rotate_req = RotateChallengesRequest(
+            self.access_token, None, [record_remove], True)
+        try:
+            gds_api.rotate_challenges(domain, rotate_req)
+        except Exception as err:
+            raise errors.PluginError(f"Unable to rotate DNS challenges: {err}")
         return
